444e1f498203ab669f06e52bd2c0b19dda7b6ce6
v-kabashnikov <vania.kabashnikov@gmail.com>
2015-12-09 21:06:39 +0300

    first commit

--- db/migrate/20151201151946_fix_series_column.rb
+++
@@ -1,5 +0,0 @@
-class FixSeriesColumn < ActiveRecord::Migration
-  def change
-    change_column :series, :title, :text
-  end
-end
--- db/migrate/20151203172450_add_columns_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :email, :text
-    add_column :users, :number, :text
-    add_column :users, :subscription, :boolean
-  end
-end
