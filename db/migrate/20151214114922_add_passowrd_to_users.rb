a8cd04fb6bf378f4d4aecd815542fea7bc0d6e64
v-kabashnikov <vania.kabashnikov@gmail.com>
2015-12-18 16:22:11 +0300

    erased migration

--- db/migrate/20151214114922_add_passowrd_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddPassowrdToUsers < ActiveRecord::Migration
-  def self.up
-    change_table(:users) do |t|
-      ## Database authenticatable
-      t.string :encrypted_password, null: false, default: ""
-    end
-
-  end
-end
